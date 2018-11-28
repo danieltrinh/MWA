54305e8caae28ebfe0319b8bd4ec8f36d2346568
Daniel <kyle.sontrinh@gmail.com>
2018-11-28 17:10:02 -0600

    Lab 2

---
+++ Lecture_2/excercise/app.js
@@ -0,0 +1,22 @@
+var http = require("http");
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello\n');
+}).listen(8081);
+
+Array.prototype.even = function(){
+    const result = this.filter((n) => n%2==0);
+    process.nextTick(() => console.log(result));
+    return result;
+};
+
+Array.prototype.odd = function(){
+    const result = this.filter((n) => n%2!=0);
+    process.nextTick(() => console.log(result));
+    return result;
+};
+
+console.log('start');
+[1,2,3,4,5,6,7,8].even();
+[1,2,3,4,5,6,7,8].odd();
+console.log('end');
