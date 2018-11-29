42e5d15ac672702992d963e0593601f5e3cf2399
Daniel <kyle.sontrinh@gmail.com>
2018-11-29 15:14:26 -0600

    Lab 3 ex 3

---
+++ lab_3/ex_3/app.js
@@ -0,0 +1,14 @@
+const EvenEmmiter = require('events');
+
+class Gym extends EvenEmmiter{
+    constructor(){
+        super();
+    }
+}
+
+const gym = new Gym();
+
+gym.on('go',() => console.log("Athlete is working out"));
+
+
+setInterval(() => gym.emit('go'), 1000);
