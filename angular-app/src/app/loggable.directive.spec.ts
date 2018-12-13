0d509f1c8ca43a6581e8f39cc067a54c19e81244
Daniel <kyle.sontrinh@gmail.com>
2018-12-12 23:38:18 -0600

    remove redundance

--- angular-app/src/app/loggable.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoggableDirective } from './loggable.directive';
-
-describe('LoggableDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoggableDirective();
-    expect(directive).toBeTruthy();
-  });
-});
