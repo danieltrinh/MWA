548139f5bfa555b4a68c19d10a37613678ce03ca
Daniel <kyle.sontrinh@gmail.com>
2018-12-15 10:02:55 -0600

    Lab 14

--- lab_14/app/signup/signup.component.ts
+++ lab_14/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  myForm : FormGroup;
+  constructor(private formBuilder : FormBuilder) {
+    this.myForm = formBuilder.group({
+      'firstname': ['Daniel', [
+        Validators.required
+      ]],
+      'lastname' : ['Trinh', [
+        Validators.required
+      ]],
+      'email' : ['abc@gmail.comg', [
+        Validators.required,
+        this.asyncEmailValidator
+      ]],
+      'password' : ['123', [
+        Validators.required
+      ]],
+      'confirmPassword' : ['123', [
+        Validators.required
+      ]]
+    });
+
+    this.myForm.valueChanges.subscribe(
+      (data:any) => console.log(data)
+    );
+  }
 
   ngOnInit() {
+  }
+
+  asyncEmailValidator(control: FormControl) : Promise<any> | Observable<any>{
+    return new Promise<any>(
+      (resolve, reject) => {
+        setTimeout(()=> {
+            if (control.value === 'abc@gmail.com')
+            {
+              console.log(control.value);
+              resolve({'invalid' : true});
+            }
+            else{
+              resolve(null);
+            }
+          }, 1500);
+      }
+    );
   }
 
 }
